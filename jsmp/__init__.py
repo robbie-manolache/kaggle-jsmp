ce8a442ada092bc87fca605ecde64258b0f167e3
robbie29 <37903975+robbie29@users.noreply.github.com>
2020-12-06 21:38:45 +1100

    create init script for jsmp modules folder

---
+++ jsmp/__init__.py
@@ -0,0 +1,4 @@
+from jsmp.helpers.config import env_config
+from jsmp.lazykaggler.competitions import competition_download, \
+    competition_files, competition_list
+from jsmp.lazykaggler.kernels import kernel_output_download
--- jsmp/lazykaggler/__init__.py
+++
@@ -1,3 +0,0 @@
-from lazykaggler.competitions import competition_list, \
-     competition_files, competition_download
-from lazykaggler.kernels import kernel_output_download
