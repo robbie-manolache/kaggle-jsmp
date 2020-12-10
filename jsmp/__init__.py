0f1f897ae7a078fcc369ad526241f716ed7b53c7
robbie29 <37903975+robbie29@users.noreply.github.com>
2020-12-10 20:29:19 +1100

    expand parquet query function to allow column/feature selection

--- jsmp/__init__.py
+++ jsmp/__init__.py
@@ -1,4 +1,4 @@
-from jsmp.query_pq import query_dates
+from jsmp.query_pq import query_train_pq
 from jsmp.helpers.config import env_config
 from jsmp.lazykaggler.competitions import competition_download, \
     competition_files, competition_list
--- jsmp/query_pq.py
+++ jsmp/query_pq.py
@@ -3,16 +3,41 @@
 
 import pyarrow.parquet as pq
 
-def query_dates(pq_dir, dmin, dmax):
+def query_train_pq(pq_dir, date_range=None, 
+                   id_cols=['date', 'ts_id'], 
+                   return_cols='all', features='all'):
     """
-    Future improvements:
-    -> allow feature/column selection
+    Allows fast, seamless querying of training data parquet database.
     """
     
+    # initialise row date filters
+    if date_range is not None:
+        filters = [('date', '>=', date_range[0]), 
+                   ('date', '<=', date_range[1])]
+    else:
+        filters = None
+    
     # establish parquet connection
-    pq_con = pq.ParquetDataset(pq_dir, filters=[('date', '>=', dmin), 
-                                                ('date', '<=', dmax)])    
+    pq_con = pq.ParquetDataset(pq_dir, filters=filters)
+    
+    # compile list of columns to query
+    all_cols = pq_con.schema.names
+    
+    # select return columns (including weight)
+    if  return_cols == "all":
+        return_cols = [c for c in all_cols if (c not in ['date', 'ts_id'])
+                       and (not c.startswith('feature_'))]
+    else:
+        pass
+    
+    # select features
+    if features == "all":
+        features = [c for c in all_cols if c.startswith('feature_')]
+    else:
+        pass
+ 
     # read data and convert to pandas
-    df = pq_con.read().to_pandas()
+    columns = id_cols + features + return_cols
+    df = pq_con.read(columns=columns).to_pandas()
     return df
 
