ff7d05c6a7a82ff8db2b9d733078b21bb18ff3db
robbie29 <37903975+robbie29@users.noreply.github.com>
2020-12-15 21:46:46 +1100

    vectorise tagging function to improve computation speed

--- jsmp/feat_eng.py
+++ jsmp/feat_eng.py
@@ -1,22 +1,29 @@
 
 ## Functions to engineer features from JSMP training data ##
 
-def assign_tag(row, tag, meta):
-    """
-    """
-    row = row[[c for c in row.index if c.startswith('feature_')]]
-    row = row.notna()
-    return all((row * meta['tag_'+str(tag)]) == meta['tag_'+str(tag)])
+import numpy as np
 
 def gen_tag_features(df, meta, verbose=False):
     """
     """
+    
+    # copy input df and create Boolean table for non-missing features
     df_out = df.copy()
-   
+    has_feat = df.copy()[[c for c in df.columns
+                          if c.startswith('feature_')]].notna()
+        
     for t in range(meta.shape[1]):
-        df_out.loc[:, 'tag_'+str(t)] = df_out.apply(assign_tag, 
-                                                    axis=1, 
-                                                    args=(t, meta))
+        
+        # matrix denoting which features must exist for this tag
+        tag_mat = np.tile(meta['tag_'+str(t)], (df.shape[0], 1))
+        
+        # if a tag feature is missing it will equal 0
+          # since F x T - T + 1 = 0
+        has_tag = (has_feat * tag_mat).astype(int) - tag_mat + 1
+
+        # if a single tag feature is missing, tag is false
+        df_out.loc[:, 'tag_'+str(t)] = has_tag.min(axis=1).astype(bool)
+        
         if verbose:
             print("Tag %d has been processed"%t)
             
