4e894c6e1fe9b6a2de15a39245190ab172379d7f
robbie29 <37903975+robbie29@users.noreply.github.com>
2021-01-17 17:00:37 +1100

    add function to categorize returns by range for classification

--- jsmp/__init__.py
+++ jsmp/__init__.py
@@ -5,7 +5,8 @@
 from jsmp.query_pq import query_train_pq
 from jsmp.feat_eng import gen_tag_features, preproc_data
 from jsmp.eval_tools import compute_utility
-from jsmp.train_prep import split_data, convert_to_lgb_dataset
+from jsmp.train_prep import gen_return_bins, split_data, \
+    convert_to_lgb_dataset
 
 # support functions
 from jsmp.helpers.config import env_config
--- jsmp/train_prep.py
+++ jsmp/train_prep.py
@@ -1,7 +1,35 @@
 
 ## Functions to prep data for training ##
 
+import numpy as np
+import pandas as pd
 import lightgbm as lgb
+
+def gen_return_bins(df, in_col='resp', out_col='resp_bin',
+                    splits=[-0.05, -0.005, 0.005, 0.05],
+                    labels=['shocker', 'will_survive', 'boring', 
+                            'not_bad', 'legendary']):
+    """
+    """
+    
+    # copy input df
+    df_out = df.copy()
+    
+    # make sure labels are the right length
+    if labels is not None:
+        if len(labels) != (len(splits) + 1):
+            print("Must specify correct number of labels!")
+            return
+
+    # pad bin splits
+    splits = [-np.inf] + splits + [np.inf]
+
+    # create categorical bin column
+    df_out.loc[:, out_col] = pd.cut(df[in_col], 
+                                    bins=splits, 
+                                    labels=labels)
+    return df_out
+    
 
 def split_data(df, 
                valid_start, valid_end="auto", 
