809e3f6922a675c737f023aa459dba6b57a65ff4
robbie29 <37903975+robbie29@users.noreply.github.com>
2021-01-18 22:13:42 +1100

    expand utility computation function to generate more performance details

--- jsmp/eval_tools.py
+++ jsmp/eval_tools.py
@@ -2,17 +2,35 @@
 ## Functions to evaluate trading performance ##
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-def compute_utility(df):
+def compute_utility(df, verbose=False, ax=None):
     """
     Note df must have 'action', 'resp', 'weight' and 'date' columns.
     TO DO:
     * expand options to provide more diagnostics.
     """
+    
+    # compute daily profit
     df_0 = df.copy()
     df_0.loc[:, 'profit'] = df['weight'] * df['resp'] * df['action'] 
     daily_profit = df_0.groupby('date')['profit'].sum()
-    t = (np.sum(daily_profit) / np.sqrt(np.sum(daily_profit ** 2))) \
-        * np.sqrt(250/len(daily_profit))
-    u = min(max(0, t), 6) * np.sum(daily_profit)
-    return u
+    
+    # compute utility and its components 
+    p = np.sum(daily_profit)
+    v = np.sqrt(np.sum(daily_profit ** 2)*len(daily_profit)/250)
+    t = p / v
+    u = min(max(0, t), 6) * p
+    
+    if verbose:      
+        print("Profit: %.2f | Volatility: %.2f | "%(p, v) +\
+              "Sharpe Ratio: %.2f | Utility: %.2f"%(t, u))
+        if ax is None:
+            _, ax = plt.subplots(figsize=(10,4))
+        else:
+            pass
+        daily_profit.plot(ax=ax)
+        plt.ylabel("Daily Profit")
+        plt.show()
+    
+    return u, daily_profit
