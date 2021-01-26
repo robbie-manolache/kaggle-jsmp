3759d5878ce35770749d3c2af40494488af549d6
robbie29 <37903975+robbie29@users.noreply.github.com>
2021-01-26 12:30:10 +1100

    add functions to make return class predictions and plot confusion matrix

--- jsmp/__init__.py
+++ jsmp/__init__.py
@@ -4,7 +4,8 @@
 # main functions
 from jsmp.query_pq import query_train_pq
 from jsmp.feat_eng import gen_tag_features, preproc_data
-from jsmp.eval_tools import compute_utility
+from jsmp.eval_tools import compute_utility, predict_return_bin, \
+    confusion_matrix
 from jsmp.train_prep import gen_return_bins, split_data, \
     convert_to_lgb_dataset
 
--- jsmp/eval_tools.py
+++ jsmp/eval_tools.py
@@ -2,7 +2,9 @@
 ## Functions to evaluate trading performance ##
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def compute_utility(df, verbose=False, ax=None):
     """
@@ -34,3 +36,54 @@ def compute_utility(df, verbose=False, ax=None):
         plt.show()
     
     return u, daily_profit
+
+def predict_return_bin(eval_df, lgb_model,
+                       eval_cols=["date", "ts_id", "weight", 
+                                  "resp", "resp_bin"]):
+    """
+    Predict return bins using LGBM classifier.
+    """
+    
+    # copy eval_df frame
+    df = eval_df.copy()[eval_cols]
+    
+    # make predictions
+    preds = lgb_model.predict(eval_df[lgb_model.feature_name()])
+    
+    # add predictions to df
+    pred_cols = ["resp_bin_" + str(i) for i in range(preds.shape[1])]
+    df.loc[:, pred_cols] = preds
+    df.loc[:, "pred_bin"] = preds.argmax(axis=1)
+    
+    return df
+
+def confusion_matrix(df, n_bins,
+                     actual="resp_bin", prediction="pred_bin",
+                     as_percent=True, ax=None):
+    """
+    Create confusion matrix to visualize classification performance.
+    """
+    
+    # create matrix
+    cfmat = pd.crosstab(df[prediction], df[actual])
+    
+    # convert to fraction
+    if as_percent:
+        cfmat = cfmat / cfmat.sum(axis=1).values.reshape((n_bins, 1))
+        vmin, vmax, fmt = 0, 1, ".2%"
+    else:
+        vmin, vmax, fmt = None, None, ".0f"
+    
+    # define ax
+    if ax is None:
+        _, ax = plt.subplots(figsize=(10,10))
+    else:
+        pass    
+    
+    # generate plot
+    sns.heatmap(cfmat, cbar=False, annot=True, fmt=fmt, 
+                ax=ax, vmin=vmin, vmax=vmax, cmap='Blues')
+    plt.xlabel("Actual Class")
+    plt.ylabel("Predicted Class")
+    plt.show()
+    
