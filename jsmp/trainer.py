0df82e00f6fcab481eda4efc41774f2e181f32c2
robbie29 <37903975+robbie29@users.noreply.github.com>
2021-01-29 22:32:46 +1100

    start build function to automate training of return classifier LGBM

--- jsmp/trainer.py
+++ jsmp/trainer.py
@@ -2,3 +2,22 @@
 ## Functions for model training ##
 
 import lightgbm as lgb
+from jsmp.query_pq import query_train_pq
+from jsmp.train_prep import split_data
+
+def train_lgb_classifier(pq_dir, train_config, label="resp_bin",
+                         ignore_cols=['date', 'ts_id', 'resp'],
+                         verbose=False):
+    """
+    """
+    
+    df = query_train_pq(pq_dir, 
+                        date_range=[train_config['date_splits'][i] 
+                                    for i in [0,2]],
+                        return_cols=['resp'])
+    
+    df_list = split_data(df, valid_start=train_config['date_splits'][1])
+    
+    return df_list
+    ## TBC ##
+    
