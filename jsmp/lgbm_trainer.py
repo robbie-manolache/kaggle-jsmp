a22b1aee87a56abd340e7113bb7d6443570edbcc
robbie29 <37903975+robbie29@users.noreply.github.com>
2021-02-13 23:43:47 +1100

    add function to create out-of-sample return class predictions for NN action model

--- jsmp/lgbm_trainer.py
+++ jsmp/lgbm_trainer.py
@@ -15,6 +15,7 @@ from jsmp.eval_tools import predict_return_bin
 def train_lgb_classifier(pq_dir, 
                          train_config, 
                          model_dir=None,
+                         model_name=None,
                          pred_dir=None,
                          label="resp_bin",
                          ignore_cols=['date', 'ts_id', 'resp'],
@@ -84,7 +85,10 @@ def train_lgb_classifier(pq_dir,
     if model_dir is None:
         pass
     else:
-        model_name = "lgbm_%s.txt"%datetime.now().strftime("%Y%m%d_%H%M%S")
+        if model_name is None:
+            model_name = "lgbm_%s.txt"%datetime.now().strftime("%Y%m%d_%H%M%S")
+        else:
+            pass
         lgb_model.save_model(os.path.join(model_dir, model_name))
         meta[model_name] = {
             'train_config': train_config,
@@ -98,9 +102,76 @@ def train_lgb_classifier(pq_dir,
     if pred_dir is None:
         pass
     else:
-        pred_df = predict_return_bin(eval_df, lgb_model)
+        pred_df = predict_return_bin(eval_df, lgb_model,
+                                     eval_cols=['date', 'ts_id'])
         table = pa.Table.from_pandas(pred_df)
         pq.write_to_dataset(table, pred_dir, partition_cols=['date'])
     
     return lgb_model
     
+def gen_date_splits(init_splits, split_config, final_splits):
+    """
+    init_splits = [start_date, valid_start, eval_start, eval_end]
+    split_config = [{'start': start_date, 
+                     'iter': number of iterations,
+                     'eval': increment to eval_end each iteration},
+                     ...]
+    final_splits = [start_date, valid_start, max_date]
+    """
+
+    # set inititial date splits
+    date_splits = [init_splits]
+    max_date = final_splits[-1]
+
+    # increment additional dates to make out-of-sample preds for
+    for sc in split_config:
+        for _ in range(sc['iter']):
+            new_splits = date_splits[-1].copy()
+            new_splits[0] = sc['start']
+            new_splits[1] = new_splits[2]
+            new_splits[2] = new_splits[3]
+            new_splits[3] = new_splits[2] + sc['eval']
+            if max(new_splits) <= max_date:
+                date_splits.append(new_splits)
+
+    # ensure that out-of-sample pred dates include max date
+    if date_splits[-1][-1] < max_date:
+        new_splits = date_splits[-1].copy()
+        new_splits[1] = new_splits[2]
+        new_splits[2] = new_splits[3]
+        new_splits[3] = max_date
+        date_splits.append(new_splits)
+
+    # add final model run dates and output date_splits
+    date_splits.append(final_splits)
+    return date_splits
+
+def multi_date_lgbm_preds(pq_dir, model_dir, pred_dir,
+                          train_config, date_splits):
+    """
+    """
+    
+    now = datetime.now().strftime("%Y%m%d_%H%M%S")
+    
+    for ds in date_splits:
+        
+        train_config["date_splits"] = ds   
+        
+        if len(ds) == 3:
+            save_preds_to = None
+            save_model_to = model_dir
+            model_name = "lgbm_%s.txt"%now 
+            print("Now training final model!")
+        else:
+            save_preds_to = os.path.join(pred_dir, "lgbm_%s"%now)
+            save_model_to = None
+            model_name=None
+            print("Now generating predictions for dates %d to %d"%
+                  (ds[2], ds[3]))
+            
+        _ = train_lgb_classifier(pq_dir=pq_dir,
+                                 model_dir=save_model_to,
+                                 model_name=model_name,
+                                 pred_dir=save_preds_to,
+                                 train_config=train_config)
+
